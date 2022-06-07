df5e3d99ea1c84ddc4050e4ceade5c7019cc5dae
Rocio Romero <rocio.aldanaromero@gmail.com>
2022-06-06 21:48:54 -0300

    Contador en el item detail

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,8 +1,35 @@
 import "./ItemDetail.css"
 import DecimalStyle from "../DecimalStyle/DecimalStyle";
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 
-const ItemDetail = ({id, name, price, img, descripcion}) => {
+const InputCount = ({onConfirm, stock, initial= 1}) => {
+    const [count, setCount] = useState(initial)
+
+    const handleChange = (e) => {
+        if(e.target.value <= stock) {
+            setCount(e.target.value)
+        }
+    }
+
+    return (
+        <div>
+            <input type='number' onChange={handleChange} value={count}/>
+            <button onClick={() => onConfirm(count)}>Agregar al carrito</button>
+        </div>
+    )
+}
+
+const ItemDetail = ({id, name, price, img, descripcion, stock}) => {
+    const [quantity, setQuantity] = useState(0)
+
+    const handleOnAdd = (count) => {
+        console.log('agregue al carrito')
+        console.log(count)
+        setQuantity(count)
+    }
+
     return (
         <div className="container item-detail">
             <div className="row">
@@ -10,6 +37,7 @@ const ItemDetail = ({id, name, price, img, descripcion}) => {
                     <img src={img} alt="" />
                 </div>
                 <div className="col-sm-4 text-left">
+
                     <div className="item-name">
                         <h1>- {name} -</h1> 
                     </div>
@@ -19,6 +47,7 @@ const ItemDetail = ({id, name, price, img, descripcion}) => {
                     <div className="item-description">
                         <p>{descripcion}</p>
                     </div>
+                    { quantity > 0  ? <Link to='/cart'>Finalizar compra</Link> : <InputCount stock={stock} onConfirm={handleOnAdd}/>}
                 </div>
             </div>
         </div>
